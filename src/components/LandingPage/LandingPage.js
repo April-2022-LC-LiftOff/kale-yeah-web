39a20f49a6a103163b3c5f77297a1a344f3956fe
Daniel <dportnoy1988@gmail.com>
2022-05-12 16:02:04 -0500

    login component

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,11 +1,14 @@
 import Features from './Features';
 import Footer from '../Navigation/Footer';
 import Welcome from './Welcome';
+import Login from '../Login/Login';
+import './LandingPage.css'
 
 const LandingPage = () => {
   return (
       <div>
       <Welcome />
+      <div className="login"><Login /></div>
       <Features />
       <Footer />
     </div>
