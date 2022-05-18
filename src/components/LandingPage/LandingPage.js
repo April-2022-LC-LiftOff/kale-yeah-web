dafd3ee326905bb934d94055196ffec0d9f4ce4f
Brando <leukocyte@protonmail.com>
2022-05-18 11:03:21 -0500

    new commit, header bar removed, trying to keep development best practices

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,12 +1,10 @@
 import Features from './Features';
 import Footer from '../Navigation/Footer';
 import Welcome from './Welcome';
-import Header from '../HeaderBar/Header';
 
 const LandingPage = () => {
   return (
       <div>
-      {/* <Header /> */}
       <Welcome />
       <Features />
       <Footer />
