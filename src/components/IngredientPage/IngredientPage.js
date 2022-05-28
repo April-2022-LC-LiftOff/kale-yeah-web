3d4ae6ce934d246b8ef2a4aa9d3c87db6179030b
Brando <leukocyte@protonmail.com>
2022-05-28 14:24:51 -0500

    axios get request returning array of objects

--- src/components/IngredientPage/IngredientPage.js
+++ src/components/IngredientPage/IngredientPage.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import { useLocation } from 'react-router-dom'
 import Header from '../HeaderBar/Header'
@@ -10,17 +10,53 @@ const IngredientPage = () => {
   
   const search = useLocation().search;
   const ingredient = new URLSearchParams(search).get('name');
+  const [data, setData] = useState([]);
 
   const fetchNameUrl = (name) => `http://localhost:8080/ingredients/${name}`;
-  const response = axios.get(fetchNameUrl(ingredient));
-  console.log(response);
+  // const data = axios.get(fetchNameUrl(ingredient)).then(response => response.data);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(fetchNameUrl(ingredient));
+        setData(response.data)
+      } catch (error) {
+        
+      }
+    }
+    fetchData();
+  }, [])
   
+  
+
+
+  // const data = response.then(response => response.data);
+  console.log(data.results);
+  
+  // if (response.data.results.length === 0) {
+  //   return (
+  //     <div className='ingredient-page'>
+  //     <Header />
+  //     <h1>Results: {ingredient}</h1>
+  //     <h2>No Results Found. Search Again</h2>
+  //     <Footer />
+  //     </div>
+  //   )
+  // }
+
   return (
     <div className='ingredient-page'>
       <Header />
       <h1>Results: {ingredient}</h1>
 
       <div className='results-container'>
+          
+          
+          
+          
+          
+          
+          
           <div className='results-box'>
           <IngredientComponent />
           </div>
