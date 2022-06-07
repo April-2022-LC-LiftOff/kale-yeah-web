d06123f740adf122bf7dd900630077147aaa7b78
Brando <leukocyte@protonmail.com>
2022-06-07 11:19:07 -0500

    create-list outline, trouble with api call

--- src/components/CreateGroceryListPage/CreateGroceryListPage.js
+++ src/components/CreateGroceryListPage/CreateGroceryListPage.js
@@ -1,10 +1,26 @@
-import React from 'react'
+import { React, useState, useEffect } from 'react'
+import axios from 'axios'
 import Header from '../HeaderBar/Header'
 import Footer from '../Navigation/Footer'
+const Image = require('../IngredientPage/placeholder-img.png')
 
 import './CreateGroceryListPage.css'
 
 const CreateGroceryListPage = () => {
+  const [ingredient, setIngredient] = useState("");
+  const [data, setData] = useState([]);
+  const fetchNameUrl = (name) => `http://localhost:8080/ingredients/${name}`;
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios.get(fetchNameUrl(ingredient));
+      setData(result.data.results);
+    };
+    fetchData(); 
+}, []);
+      
+  
+  
   return (
     <div className='create-list'>
       <Header />
@@ -22,11 +38,71 @@ const CreateGroceryListPage = () => {
             <input 
             type='text'/>
           </div>
-
-          <div className='items'><p>Items:</p></div>
-          
-          
       </div>
+
+      <div className='list-items'>
+
+        <div className='items'><p>Items:</p></div>
+
+        {/* need a for each loop here that dynamically displays
+        what items are currently in the Grocery List
+        Should be checkboxes that allow for deletion */}
+
+        <div className='list-btns'>
+
+          <div id='delete'>
+            <button>
+               Delete Items
+            </button>
+            </div>
+
+            <div id='save'>
+            <button>
+               Save List
+            </button>
+            </div>
+
+        </div>
+      </div>
+
+
+      <h2>Search For Ingredients:</h2>
+      <div className='ingredient-search'>
+        <form onSubmit={console.log(data)}>
+        <input 
+              className='ing-search-box'
+              type="text" 
+              placeholder="Search"
+              value={ingredient}
+              onChange={(e) => setIngredient(e.target.value)}
+              />
+        </form>
+        </div>
+
+        <div className='ing-search-results'>
+          {data != 0 ? () => {
+            return data.map((item) => {
+              return <div className='ing-component'>
+              <h2>{item.name}</h2>
+              <img className="ing-img" src={Image} alt={item.name}></img>
+              <p>Price: $9.99</p>
+              <p>Calories: 2500</p>
+              
+              <div id='save'>
+                <button>
+                 Add To List
+                </button>
+              </div>
+        
+            </div>
+            })
+
+          } : null}
+        </div>
+          
+          
+          
+      
 
 
     <Footer />
