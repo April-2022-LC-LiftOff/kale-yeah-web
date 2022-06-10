327065577ab0e5de66301363f2aa58d10961b6e7
Brando <leukocyte@protonmail.com>
2022-06-10 10:33:38 -0500

    react icons added

--- src/components/CreateGroceryListPage/CreateGroceryListPage.js
+++ src/components/CreateGroceryListPage/CreateGroceryListPage.js
@@ -1,4 +1,5 @@
-import { React, useState } from 'react'
+import { React, useState, useContext } from 'react'
+import { useNavigate } from 'react-router-dom'
 import axios from 'axios'
 import Header from '../HeaderBar/Header'
 import Footer from '../Navigation/Footer'
